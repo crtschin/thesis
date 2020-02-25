5a984d87c9422c69c02d592ddccda4cfcc7120de
crtschin <csochinjensem@gmail.com>
2020-02-25 15:22:03 +0100

    Filled out denotation

--- Direct.v
+++ Direct.v
@@ -41,25 +41,28 @@ Fixpoint denote_ctx (Γ : Ctx) : Type :=
 where "⟦ T ⟧ₜₓ" := (denote_ctx T).
 
 Check snd.
-Program Fixpoint denote_v {Γ τ} (v: τ ∈ Γ) : denote_ctx Γ -> denote_t τ  :=
+Program Fixpoint denote_v {Γ τ} (v: τ ∈ Γ) : ⟦Γ⟧ₜₓ -> ⟦τ⟧ₜ  :=
   match v with
   | Top Γ' τ' => fun gamma => fst gamma
-  | Pop Γ' τ' σ x => fun _ => ⟦ τ ⟧ₜ
+  | Pop Γ' τ' σ x => fun gamma => denote_v x (snd gamma)
   end.
+Notation "⟦ v ⟧ᵥ" := (denote_v v).
 
-Fixpoint denote_tm Γ τ (t : tm Γ τ) : ⟦Γ⟧ₜₓ -> ⟦τ⟧ₜ :=
+Reserved Notation "⟦ t ⟧ₜₘ".
+Program Fixpoint denote_tm {Γ τ} (t : tm Γ τ) : ⟦Γ⟧ₜₓ -> ⟦τ⟧ₜ :=
   match t with
-  | const _ r         => (fun ctx => _)
-  | add _ t1 t2       => (fun ctx => _)
+  | var _ σ v => fun ctx => denote_v v ctx
+  | app _ σ ρ t1 t2 => fun ctx => (⟦t1⟧ₜₘ ctx) (⟦t2⟧ₜₘ ctx)
+  | abs _ σ ρ f => fun ctx => fun x => ⟦ f ⟧ₜₘ (x, ctx)
 
-  | var _ _ v         => (fun ctx => _)
-  | app _ _ _ t1 t2   => (fun ctx => _)
-  | abs _ _ _ f       => (fun ctx => _)
+  | const _ r => fun ctx => r
+  | add _ t1 t2 => fun ctx => ⟦t1⟧ₜₘ ctx + ⟦t2⟧ₜₘ ctx
 
-  | tuple _ _ _ t1 t2 => (fun ctx => _)
-  | first _ _ _ p       => (fun ctx => _)
-  | second _ _ _ p       => (fun ctx => _)
-  end.
+  | tuple _ σ ρ t1 t2 => fun ctx => (⟦t1⟧ₜₘ ctx, ⟦t2⟧ₜₘ ctx)
+  | first _ σ ρ t => fun ctx => fst (⟦t⟧ₜₘ ctx)
+  | second _ σ ρ t => fun ctx => snd (⟦t⟧ₜₘ ctx)
+  end
+where "⟦ t ⟧ₜₘ" := (denote_tm t).
 
 Record Gl := make_gl {
   X : ty;
@@ -70,6 +73,7 @@ Record Gl := make_gl {
 (*
   Relation used by Gl giving a notion of
     differentiability between ⟦τ⟧ₜ and ⟦σ⟧ₜ
+    Maybe not needed or wrong?
 *)
 Inductive diff : forall τ σ, (R -> ⟦τ⟧ₜ) -> (R -> ⟦σ⟧ₜ) -> Prop :=
   | diff_r : diff Real (Dt Real) id (fun r => (r, r))
