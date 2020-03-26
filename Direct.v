bfd899e8f7b068b32a458f92ee1db28e8e14184e
crtschin <csochinjensem@gmail.com>
2020-03-26 11:05:41 +0100

    Update

--- Direct.v
+++ Direct.v
@@ -36,39 +36,39 @@ Local Open Scope R_scope.
     denotation function of macro term
       (the derivative of the function the term describes)
 *)
-Equations S Γ τ : tm Γ τ -> (R -> ⟦ τ ⟧ₜ) -> (R -> ⟦ Dt τ ⟧ₜ) -> Prop :=
-S Γ Real t f g :=
+Equations S Γ τ (h1 : R -> ⟦ Γ ⟧ₜₓ) (h2 : R -> ⟦ Dctx Γ ⟧ₜₓ) : tm Γ τ -> (R -> ⟦ τ ⟧ₜ) -> (R -> ⟦ Dt τ ⟧ₜ) -> Prop :=
+S Γ Real h1 h2 t f g :=
   (*
   /\ *)
-  forall (h1 : R -> ⟦ Γ ⟧ₜₓ) (h2 : R -> ⟦ Dctx Γ ⟧ₜₓ),
   (forall (x : R), ex_derive (⟦ t ⟧ₜₘ ∘ h1) x) /\
   (fun r => (⟦ Dtm t ⟧ₜₘ ∘ h2) r) = (fun r => ((⟦ t ⟧ₜₘ ∘ h1) r, Derive (⟦ t ⟧ₜₘ ∘ h1) r));
-S Γ (σ × ρ) t f g :=
+S Γ (σ × ρ) h1 h2 t f g :=
   exists f1 f2 g1 g2 t1 t2,
-  exists (s1 : S Γ σ t1 f1 f2) (s2 : S Γ ρ t2 g1 g2),
+  exists (s1 : S Γ σ h1 h2 t1 f1 f2) (s2 : S Γ ρ h1 h2 t2 g1 g2),
     t = tuple Γ t1 t2 /\
     (f = fun r => (f1 r, g1 r)) /\
     (g = fun r => (f2 r, g2 r));
-S Γ (σ → ρ) t f g :=
+S Γ (σ → ρ) h1 h2 t f g :=
   (* exists h, *)
   forall (s : tm Γ σ),
   forall g1 g2,
-  forall (sσ : S Γ σ s g1 g2),
+  forall (sσ : S Γ σ h1 h2 s g1 g2),
     (* (fun x => h1 x) = (fun x => f x (g1 x)) /\
     (fun x => h2 x) = (fun x => g x (g2 x)); *)
-    (* value s -> *)
+    value s ->
     (* g1 = (⟦s⟧ₜₘ ∘ denote_env ∘ h) -> *)
     (* g2 = (⟦Dtm s⟧ₜₘ ∘ denote_env ∘ Denv ∘ h) -> *)
-    S Γ ρ (app Γ _ _ t s) (fun x => f x (g1 x)) (fun x => g x (g2 x));
-S Γ (σ <+> ρ) t f g :=
+    S Γ ρ h1 h2 (app Γ _ _ t s) (fun x => f x (g1 x))
+      (fun x => g x (g2 x));
+S Γ (σ <+> ρ) h1 h2 t f g :=
   (exists g1 g2,
     forall (t : tm Γ σ),
-    forall (s: S Γ σ t g1 g2),
+    forall (s: S Γ σ h1 h2 t g1 g2),
       f = Datatypes.inl ∘ g1 /\
       g = Datatypes.inl ∘ g2) \/
   (exists g1 g2,
     forall (t : tm Γ ρ),
-    forall (s: S Γ ρ t g1 g2),
+    forall (s: S Γ ρ h1 h2 t g1 g2),
       f = Datatypes.inr ∘ g1 /\
       g = Datatypes.inr ∘ g2).
 
@@ -88,7 +88,7 @@ Inductive instantiation :
         forall {t : tm Γ' τ},
       instantiation s ->
         value t ->
-        (forall g1 g2, S Γ' τ t g1 g2) ->
+        (forall h1 h2, S Γ' τ h1 h2 t (⟦ t ⟧ₜₘ ∘ h1) (⟦ Dtm t ⟧ₜₘ ∘ h2)) ->
         instantiation (cons_sub t s).
 
 (* Equations instantiation Γ {Γ'} (f : R -> Env Γ') : sub Γ Γ' -> Prop :=
@@ -100,16 +100,17 @@ instantiation (τ :: Γ) f s :=
       g2 = (⟦Dtm (hd_sub s)⟧ₜₘ ∘ denote_env ∘ Denv ∘ f) /\
       instantiation Γ f (tl_sub s). *)
 
-Lemma S_cong : forall Γ τ t f1 f2 g1 g2,
-  g1 = f1 -> g2 = f2 -> S Γ τ t f1 f2 = S Γ τ t g1 g2.
+Lemma S_cong : forall Γ τ t f1 f2 g1 g2 h1 h2,
+  g1 = f1 -> g2 = f2 -> S Γ τ h1 h2 t f1 f2 = S Γ τ h1 h2 t g1 g2.
 Proof. intros. rewrites. Qed.
 
-Lemma S_soundness : forall Γ τ (t t' : tm Γ τ) f f',
+Lemma S_soundness : forall Γ τ (t t' : tm Γ τ) f f' h1 h2,
   (t -->* t') ->
-    S Γ τ t f f' = S Γ τ t' f f'.
+    S Γ τ h1 h2 t f f' = S Γ τ h1 h2 t' f f'.
 Proof with quick.
   intros.
-  induction τ; simp S...
+  induction H; simp S...
+  rewrite <- IHmulti.
   all: admit.
   (* intros.
   pose proof (soundness _ _ t t' H) as Heq.
@@ -144,14 +145,15 @@ Proof.
   exists (const Γ 0). split.
 Admitted. *)
 
-Lemma S_app : forall Γ τ σ t1 t2 h1 h1' h2 h2',
+(* Lemma S_app : forall Γ τ σ t1 t2 h1 h1' h2 h2',
   S Γ σ t2 h2 h2' ->
     S Γ (σ → τ) t1 h1 h1' ->
-    S Γ τ (app Γ τ σ t1 t2) (fun r => (h1 r) (h2 r)) (fun r => (h1' r) (h2' r)).
+    S Γ τ (app Γ τ σ t1 t2) (fun r => (h1 r) (h2 r))
+    (fun r => (h1' r) (h2' r)).
 Proof with quick.
   intros...
   simp S in H0.
-Admitted.
+Admitted. *)
 
 
 (*
@@ -164,7 +166,8 @@ Lemma fundamental :
   forall Γ Γ' τ
     (t : tm Γ τ) (sb : sub Γ Γ') h1 h2,
   instantiation sb ->
-  S Γ' τ (substitute sb t) (⟦substitute sb t⟧ₜₘ ∘ h1) (⟦Dtm (substitute sb t)⟧ₜₘ ∘ h2).
+  S Γ' τ h1 h2 (substitute sb t) (⟦substitute sb t⟧ₜₘ ∘ h1)
+    (⟦Dtm (substitute sb t)⟧ₜₘ ∘ h2).
 Proof with quick.
   intros Γ Γ' τ t sb h1 h2 H.
   generalize dependent Γ'.
@@ -217,7 +220,12 @@ Proof with quick.
   { (* Abs *)
     intros. simp S. intros; subst...
     simpl (substitute sb (abs Γ τ σ t)).
-    simp Dtm. simpl.
+    simp Dtm. simpl. rewrites.
+    erewrite S_soundness.
+  2:{ apply multistep_AppAbs... constructor. }
+    rewrite <- app_sub_sub.
+    pose proof (compose_sub_sub (| s |) (substitute_lifted sb)).
+
     exists (⟦ s ⟧ₜₘ ∘ h1).
     exists (⟦ Dtm s ⟧ₜₘ ∘ h2).
     exists (⟦ substitute (cons_sub s sb) t ⟧ₜₘ ∘ h1).
